dd9f46d04dc88e8ba7712cf2c0f5af3d0ef51290
David Benček <david.bencek@ifw-kiel.de>
2016-02-15 14:46:20 +0100

    renamed package and data, updated documentation

---
+++ R/arvig.R
@@ -0,0 +1,20 @@
+#' 1645 events of anti-refugee violence in Germany in 2014 and 2015.
+#'
+#' A dataset containing georeferenced events of anti-refugee violence in
+#' Germany during the years 2014 and 2015.
+#'
+#' @format A data frame with 1645 rows and 10 variables:
+#' \describe{
+#'   \item{date}{date of event}
+#'   \item{location}{city or town of event as reported}
+#'   \item{state}{state of event}
+#'   \item{community_id}{community identification number of reported city/town as used by statistical offices}
+#'   \item{longitude}{longitude of event}
+#'   \item{latitude}{latitude of event}
+#'   \item{category_de}{type of event (German)}
+#'   \item{category_en}{type of event (English)}
+#'   \item{description}{short summary of event}
+#'   \item{source}{primary source}
+#' }
+#' @source \url{https://www.mut-gegen-rechte-gewalt.de}
+"arvig"
